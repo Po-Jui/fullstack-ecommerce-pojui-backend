838fb31c927e60a0ee8090efefa70511155cf7ef
PoJui Chen <a0970872656@gmail.com>
2024-08-26 13:03:41 +0800

    newebpay test

--- routes/user/orders.js
+++ routes/user/orders.js
@@ -272,15 +272,16 @@ router.post("/createOrder", (req, res) => {
   }
 });
 
-// 交易成功：Return （可直接解密，將資料呈現在畫面上）
+// 交易成功：Return
 router.post("/newebpay_return", async (req, res, next) => {
   console.log("req.body return data", req.body);
   const response = req.body;
+
   // 解密交易內容
   const data = createSesDecrypt(response.TradeInfo);
   console.log("data:", data.Result.MerchantOrderNo);
 
-  if (data.Status === "SUCCESS") {
+  if (data.Status == "SUCCESS") {
     try {
       const orderId = data.Result.MerchantOrderNo;
       const orderRef = db.collection("orders").doc(orderId);
@@ -296,20 +297,21 @@ router.post("/newebpay_return", async (req, res, next) => {
         paid_date: admin.firestore.Timestamp.now().seconds, // 新增付款日期
       });
 
-      // res.json({
-      //   success: true,
-      //   message: "付款完成",
-      // });
+      // 渲染結果頁面
+      res.render("success", {
+        title: "Express",
+        redirectUrl: `/checkout/${data.Result.MerchantOrderNo}`,
+      });
     } catch (error) {
       res.status(500).json({ success: false, message: error.message });
     }
+  } else {
+    // 如果交易不成功，渲染錯誤頁面或其他處理
+    res.render("failure", {
+      title: "交易失敗",
+      message: "交易未成功，請稍後再試。",
+    });
   }
-
-  // 渲染結果頁面
-  res.render("success", {
-    title: "Express",
-    redirectUrl: `${redirectUrl}/${data.Result.MerchantOrderNo}`,
-  });
 });
 
 // 確認交易：Notify
