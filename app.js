d51c31b36f020dc5e3b7d749fa70ee11f8adc9b6
PoJui Chen <a0970872656@gmail.com>
2024-08-28 06:05:18 +0800

    modify some code

--- app.js
+++ app.js
@@ -86,6 +86,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use((err, req, res, next) => {
-  console.error(err.stack);
+  // console.error(err.stack);
   res.status(500).send("Something broke!");
 });
